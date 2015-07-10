f87ff8baf6506c21a5fa106dda036d776aa28f75
Rodrigo Ariel Seoane Quilne <rseoane@ws22.labo4>
2015-07-10 18:42:27 -0300

    mitad del driver

--- driver-test-cpp/Driver.cpp
+++ driver-test-cpp/Driver.cpp
@@ -1,67 +1,101 @@
 #include "Driver.h"
-#include "ArbolSintactico.h"
+#include "TiposBasicos.h"
 
 namespace aed2 {
 
 Driver::Driver() {
-    // TODO
+    lared = new  Red();
+    eldcnet = new DCNet(lared);
 }
 
 Driver::~Driver() {
-    // TODO
+    delete lared;
+    delete eldcnet;
 }
 
 // TAD RED
 Nat Driver::CantidadComputadoras() const {
-    // TODO
-    return 0;
+    Conj<Compu>::Iterador it = (lared->Computadoras()).CrearIt();
+    int res = 0;
+        while(it.HaySiguiente()){
+            res ++;
+            it.Avanzar();
+        }
+     return res;
 
 }
 
 const Computadora& Driver::IesimaComputadora(const Nat i) const {
-    // TODO
-	return 0;
+    Conj<Compu>::Iterador it = (lared->Computadoras()).CrearIt();
+    int n = 0;
+    Computadora res;
+        while(it.HaySiguiente()){
+            if(n == i){
+                return res = it.Siguiente();
+            }
+            i ++;
+            it.Avanzar();
 }
         
 Nat Driver::CantidadInterfacesDe(const Computadora& c) const {
-    // TODO
-    return 0;
-
+    Conj<Compu>::Iterador it = (lared->Computadoras()).CrearIt();
+        while((it.Siguiente()).hostname != c){
+              
+            it.Avanzar();
+        }
+    return ((it.Siguiente()).interfaces).Cardinal();
 }
 
 const Interfaz& Driver::IesimaInterfazDe(const Computadora& c, const Nat i) const{
-    // TODO
-    return 0;
+    Conj<Compu>::Iterador it = (lared->Computadoras()).CrearIt();
+    Conj<Interfaz> cji;
+    
+    while((it.Siguiente()).hostname != c){
+        it.Avanzar();
+    }
+    cji = ((it.Siguiente()).interfaces);
+    Conj<Interfaz>::Iterador itint = cji.CrearIt();
+    int n = 0
+    Interfaz res;
 
-} 
+    while(itint.HaySiguiente()){
+        if(n == i){
+            return res = itint.Siguiente();
+        }
+        i++;
+        it.Avanzar();
+
+    }
+}
+
 
 const Interfaz& Driver::IntefazUsada(const Computadora& c1, const Computadora& c2) const {
-    // TODO
-    return 0;
-
+    
+     return lared.InterfazUsada(c1, c2);
+    
 }
 
 bool Driver::conectadas(const Computadora& c1, const Computadora& c2) const {
-    // TODO
-    return 0;
+    
+    return lared.Conectadas(c1,c2);
 }
 
 // TAD DCNET
 void Driver::AgregarComputadora(const Computadora& ip, const Conj<Interfaz>& ci) {
-    // TODO
+    ::Compu 
 }
         
 void Driver::Conectar(const Computadora& c1, const Interfaz& i1, const Computadora& c2, const Interfaz& i2) {
     // TODO
 }
-	
-	
+    
+    
 Nat Driver::CantidadNodosRecorridosPor(const Paquete& p) const {
     // TODO
     return 0;
 }
 
-const Computadora& Driver::IesimoNodoRecorridoPor(const Paquete& p, const Nat i) const {
+const Computadora& Driver::IesimoNodoRecorridoPor(const Paquete& p) const {
     // TODO
 }
 
@@ -69,13 +103,13 @@ Nat Driver::CantidadEnviadosPor(const Computadora& c) const {
     // TODO
     return 0;
 }
-	
+    
 Nat Driver::CantidadEnEsperaEn(const Computadora& c) const {
     // TODO
     return 0;
 }
 
-const Paquete& Driver::IesimoEnEsperaEn(const Computadora& c, const Nat i) const {
+const Paquete& Driver::IesimoEnEsperaEn(const Computadora& c) const {
     // TODO
     return 0;
 }
@@ -83,31 +117,31 @@ const Paquete& Driver::IesimoEnEsperaEn(const Computadora& c, const Nat i) const
 void Driver::CrearPaquete(const Computadora& origen, const Computadora& destino, Nat prioridad) {
     // TODO
 }
-		
+        
 void Driver::AvanzarSegundo() {
     // TODO
 }
-		
+        
 const Computadora& Driver::laQueMasEnvio() const {
-    // TODO	
+    // TODO 
     return 0;
 }
 
 const Computadora& Driver::origen(const Paquete& p) const {
-    // TODO	
+    // TODO 
     return 0;
 } 
 
 const Computadora& Driver::destino(const Paquete& p) const { 
-    // TODO	
+    // TODO 
     return 0;
 }
 
 Nat Driver::prioridad(const Paquete& p) const { 
-    // TODO	
+    // TODO 
     return 0;
 }
-		
+        
 
 
 } // namespace aed2
